536ebcbacbc338abaa1154e0275405a8b98574fd
kajal gupta <kajalg1401@gmail.com>
2024-07-16 09:00:33 +0530

    done

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -42,8 +42,8 @@ router.post('/login', async (req, res) => {
   }
 });
 
-router.get('/register', (req, res) => {
-  res.sendFile(path.join(__dirname, '../../client', 'register.html'));
+router.get('/login', (req, res) => {
+  res.sendFile(path.join(__dirname, '../../client', 'login.html'));
 });
 
 module.exports = router;
